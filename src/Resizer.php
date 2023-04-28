1bfc821565ca1416542e54c9aaa3d2695612e090
tomkirsch <mrmotts@gmail.com>
2023-04-28 15:20:52 -0400

    strong typing, cleanup

--- src/Resizer.php
+++ src/Resizer.php
@@ -1,83 +1,88 @@
-<?php namespace Tomkirsch\Resizer;
+<?php
+
+namespace Tomkirsch\Resizer;
 
 /*
 	Automagically resize a source image with caching
 	If .htaccess doesn't seem to want to recognize the path, try setting CI's App config $uriProtocol to PATH_INFO
 */
 
-class Resizer{
+class Resizer
+{
 	public $config;
 	public $imageLib;
-	
-	public function __construct($config){
+
+	public function __construct($config)
+	{
 		$this->config = $config;
 	}
-	
+
 	// reads a certain size image and outputs contents. If the source is newer than the cache, it is automatically cleaned
-	public function read(string $imageFile, int $size, string $ext='.jpg'){
+	public function read(string $imageFile, int $size, string $ext = '.jpg')
+	{
 		// random clean
-		if($this->config->useCache && rand(1,$this->config->randomCleanChance) === 1){
+		if ($this->config->useCache && rand(1, $this->config->randomCleanChance) === 1) {
 			$this->cleanDir(FALSE);
 		}
 		// load GD image lib
-		if(!$this->imageLib) $this->imageLib = \Config\Services::image('gd');
+		if (!$this->imageLib) $this->imageLib = \Config\Services::image('gd');
 		$sourceFile = $this->sourceFile($imageFile, $ext);
 		$cacheFile = $this->cacheFile($imageFile, $size, $ext);
 		$createCache = FALSE;
 		$sourceTime = filemtime($sourceFile);
-		
-		if(!file_exists($cacheFile)){
+
+		if (!file_exists($cacheFile)) {
 			$createCache = $this->config->useCache;
 			$cacheExists = FALSE;
-		}else{
+		} else {
 			// compare filemtime
-			if($sourceTime > filemtime($cacheFile)){
+			if ($sourceTime > filemtime($cacheFile)) {
 				// source was updated, clear cache
-				try{
+				try {
 					unlink($cacheFile);
-				}catch(\Exception $e){
-					log_message('error', 'Resizer Lib cannot unlink file '.$cacheFile);
+				} catch (\Exception $e) {
+					log_message('error', 'Resizer Lib cannot unlink file ' . $cacheFile);
 				}
 				$createCache = $this->config->useCache;
 				$cacheExists = FALSE;
-			}else{
+			} else {
 				$cacheExists = TRUE;
 			}
 		}
-		
-		if(!$cacheExists){
+
+		if (!$cacheExists) {
 			$this->imageLib
 				->withFile($sourceFile)
-				->resize($size, $size, TRUE, 'width')
-			;
-			if($createCache){
+				->resize($size, $size, TRUE, 'width');
+			if ($createCache) {
 				$this->imageLib->save($cacheFile);
 				$cacheExists = TRUE;
 			}
 		}
-		
+
 		// remove any CI output buffering
 		ob_end_flush();
-		
+
 		// generate headers
 		$extNoDot = substr($ext, 1);
 		$mime = $this->mimeFromExt($extNoDot);
-		if($mime) header("Content-Type: $mime");
+		if ($mime) header("Content-Type: $mime");
 		$headerDate = gmdate('D, d M Y H:i:s T', $sourceTime);
 		header("Last-Modified: $headerDate");
 		header("Cache-control: public, max-age=2592000"); // 1 mo.
 		// if we have a cache file, read it
-		if($cacheExists){
-			header("Content-Length: ".filesize($cacheFile));
+		if ($cacheExists) {
+			header("Content-Length: " . filesize($cacheFile));
 			readfile($cacheFile);
-		}else{
+		} else {
 			// image resource should still exist, output it to the browser using GD function
 			$this->outputResource($extNoDot);
 		}
 	}
-	
+
 	// cleans the entire cache dir. Uses filemtime or forced deletion
-	public function cleanDir(bool $forceCleanAll=FALSE){
+	public function cleanDir(bool $forceCleanAll = FALSE): array
+	{
 		// interator
 		$dir = new \RecursiveDirectoryIterator($this->config->resizerCachePath);
 		$filter = new \RecursiveCallbackFilterIterator($dir, function ($current, $key, $iterator) {
@@ -91,91 +96,111 @@ class Resizer{
 		foreach ($iterator as $info) {
 			// delete file if $forceCleanAll, or file is older than our ttl
 			$expired = $this->config->ttl ? $info->getMTime() + $this->config->ttl > time() : FALSE;
-			if($forceCleanAll || $expired){
-				try{
-					unlink($info->getRealPath());
-				}catch(\Exception $e){
-					log_message('error', 'Resizer Lib cannot unlink file '.$info->getRealPath());
+			if ($forceCleanAll || $expired) {
+				$file = $info->getRealPath();
+				try {
+					unlink($file);
+					$files[] = $file;
+				} catch (\Exception $e) {
+					log_message('error', 'Resizer Lib cannot unlink file ' . $file);
 				}
 			}
 		}
+		return $files;
 	}
-	
+
 	// utility - removes all cached files for a given image
-	public function cleanFile(string $imageFile){
+	public function cleanFile(string $imageFile): array
+	{
 		$dir = new \RecursiveDirectoryIterator($this->config->resizerCachePath);
-		$filter = new \RecursiveCallbackFilterIterator($dir, function ($current, $key, $iterator) {
+		$filter = new \RecursiveCallbackFilterIterator($dir, function ($current, $key, $iterator) use ($imageFile) {
 			// Skip hidden files and directories.
 			if ($current->getFilename()[0] === '.') return FALSE;
 			if ($current->isDir()) return TRUE; // recursive
 			// only delete files with the given filename
 			return stristr($current->getFilename(), $imageFile);
 		});
-		$iterator = new \RecursiveIteratorIterator($filter);
 		$files = [];
+		$iterator = new \RecursiveIteratorIterator($filter);
 		foreach ($iterator as $info) {
-			try{
-				unlink($info->getRealPath());
-			}catch(\Exception $e){
-				log_message('error', 'Resizer Lib cannot unlink file '.$info->getRealPath());
+			$file = $info->getRealPath();
+			try {
+				unlink($file);
+				$files[] = $file;
+			} catch (\Exception $e) {
+				log_message('error', 'Resizer Lib cannot unlink file ' . $file);
 			}
 		}
+		return $files;
 	}
-	
+
 	// utility - generate a public-pointing file path
-	public function publicFile(string $imageFile, int $size, string $ext='.jpg'):string{
-		return $this->config->rewriteSegment.'/'.$imageFile.$this->config->rewriteSizeSep.$size.$ext;
+	public function publicFile(string $imageFile, int $size, string $ext = '.jpg'): string
+	{
+		return $this->config->rewriteSegment . '/' . $imageFile . $this->config->rewriteSizeSep . $size . $ext;
 	}
-	
+
 	// utility - gets the path + name of the source image file
-	public function sourceFile(string $imageFile, string $ext='.jpg'):string{
-		return $this->config->realImagePath.'/'.$imageFile.$ext;
+	public function sourceFile(string $imageFile, string $ext = '.jpg'): string
+	{
+		return $this->config->realImagePath . '/' . $imageFile . $ext;
 	}
-	
+
 	// utility - gets the path + name of the cache file, and ensures the path exists
-	public function cacheFile(string $imageFile, int $size, string $ext='.jpg'):string{
+	public function cacheFile(string $imageFile, int $size, string $ext = '.jpg'): string
+	{
 		// ensure source file exists before we create directories!
 		$sourceFile = $this->sourceFile($imageFile, $ext);
-		if(!is_file($sourceFile)){
+		if (!is_file($sourceFile)) {
 			throw new \Exception("Cannot find source file $sourceFile");
 		}
 		$cacheDir = $this->config->resizerCachePath;
-		$cacheFileName = $imageFile.'-'.$size.$ext;
+		$cacheFileName = $imageFile . '-' . $size . $ext;
 		$cacheFile = "$cacheDir/$cacheFileName";
 		$pathinfo = pathinfo($cacheFile);
-		if(!is_dir($pathinfo['dirname'])){
+		if (!is_dir($pathinfo['dirname'])) {
 			mkdir($pathinfo['dirname'], 0777, TRUE);
 		}
 		return $cacheFile;
 	}
-	
-	protected function mimeFromExt(string $ext):?string{
+
+	protected function mimeFromExt(string $ext): ?string
+	{
 		$mime = NULL;
-		switch(strtolower($ext)){
+		switch (strtolower($ext)) {
 			case 'jpg':
 			case 'jpeg':
-				$mime = image_type_to_mime_type(IMAGETYPE_JPEG); break;
+				$mime = image_type_to_mime_type(IMAGETYPE_JPEG);
+				break;
 			case 'png':
-				$mime = image_type_to_mime_type(IMAGETYPE_PNG); break;
+				$mime = image_type_to_mime_type(IMAGETYPE_PNG);
+				break;
 			case 'webp':
-				$mime = image_type_to_mime_type(IMAGETYPE_WEBP); break;
+				$mime = image_type_to_mime_type(IMAGETYPE_WEBP);
+				break;
 			case 'gif':
-				$mime = image_type_to_mime_type(IMAGETYPE_GIF); break;
+				$mime = image_type_to_mime_type(IMAGETYPE_GIF);
+				break;
 		}
 		return $mime;
 	}
-	
-	protected function outputResource(string $ext){
-		switch(strtolower($ext)){
+
+	protected function outputResource(string $ext)
+	{
+		switch (strtolower($ext)) {
 			case 'jpg':
 			case 'jpeg':
-				$func = 'imagejpeg'; break;
+				$func = 'imagejpeg';
+				break;
 			case 'png':
-				$func = 'imagepng'; break;
+				$func = 'imagepng';
+				break;
 			case 'webp':
-				$func = 'imagewebp'; break;
+				$func = 'imagewebp';
+				break;
 			case 'gif':
-				$func = 'imagegif'; break;
+				$func = 'imagegif';
+				break;
 		}
 		$func($this->imageLib->getResource());
 	}
--- src/ResizerConfig.php
+++ src/ResizerConfig.php
@@ -1,14 +1,17 @@
-<?php namespace Tomkirsch\Resizer;
+<?php
+
+namespace Tomkirsch\Resizer;
 
 use CodeIgniter\Config\BaseConfig;
 
-class ResizerConfig extends BaseConfig{
-	public $rewriteSegment = 'imagerez'; // this "folder" lets htaccess know to rewrite the request to Resizer controller. must match your .htaccess file regex
+class ResizerConfig extends BaseConfig
+{
+	public string $rewriteSegment = 'imagerez'; // this "folder" lets htaccess know to rewrite the request to Resizer controller. must match your .htaccess file regex
 	// RewriteRule ^imagerez\/(.+)-([0-9]+)\.(.+) resizer/read?file=$1&size=$2&ext=$3 [NC,QSA]
-	public $rewriteSizeSep = '-'; // separator from base file name and requested size. must match your .htaccess file regex
-	public $realImagePath = ROOTPATH . '/public'; // real path to source images
-	public $useCache = TRUE; // turn off file cache for testing
-	public $resizerCachePath = ROOTPATH . '/writable/resizercache'; // path to store cached image files
-	public $ttl = 60 * 60 * 24 * 7; // clean cached images older than this (seconds)
-	public $randomCleanChance = 100; // library will auto clean cache folder upon file read - helps clean cache of deleted images
+	public string $rewriteSizeSep = '-'; // separator from base file name and requested size. must match your .htaccess file regex
+	public string $realImagePath = ROOTPATH . '/public'; // real path to source images
+	public bool $useCache = TRUE; // turn off file cache for testing
+	public string $resizerCachePath = ROOTPATH . '/writable/resizercache'; // path to store cached image files
+	public int $ttl = 60 * 60 * 24 * 7; // clean cached images older than this (seconds)
+	public int $randomCleanChance = 100; // library will auto clean cache folder upon file read - helps clean cache of deleted images
 }
